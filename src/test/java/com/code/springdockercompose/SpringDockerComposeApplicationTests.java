dbbae2e8342522d7a05e4f296ec5ef0fbca2e931
Ruslan Balkovyy <rbalkovyy@gmail.com>
2022-10-06 21:01:15 +0300

    initial commit

---
+++ src/test/java/com/code/springdockercompose/SpringDockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.springdockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerComposeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
